4742b7cf81e95958b0ddf5e2a1c70a31478b38b1
Charles Wesley <cwesley@uw.edu>
2016-05-03 13:43:45 -0700

    Added json call to gsheet

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,3 @@
-$("#projects").sheetrock({
-  url: "https://docs.google.com/spreadsheets/d/1XVzbRxvQ-r_Sijs-mtSfdjghsZ6QV7lFi-U-kW0FgrI/",
-  query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O order by D"
+$('#projects').sheetrock({
+  url: "https://docs.google.com/spreadsheets/d/1XVzbRxvQ-r_Sijs-mtSfdjghsZ6QV7lFi-U-kW0FgrI/edit#gid=0"
 });
