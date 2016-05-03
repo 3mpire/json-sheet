db76cfca254692576fa59e7df3c74881d7088a0c
Charles Wesley <cwesley@uw.edu>
2016-05-03 13:32:04 -0700

    Added javascript for app

---
+++ js/projects.js
@@ -0,0 +1,4 @@
+$("#projects").sheetrock({
+  url: "https://docs.google.com/spreadsheets/d/1XVzbRxvQ-r_Sijs-mtSfdjghsZ6QV7lFi-U-kW0FgrI/",
+  query: "select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O order by D"
+});
